33bbcf239766cc64e72129f65a775a118d388d14
taku629 <karisutefin73@gmail.com>
2026-01-20 01:19:03 +0900

    Creating the UI foundation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {use, useStates} from "react";
+export default function App(){
+  const [city, setCity] = useState("Tokyo");
+  const [weater, setWeather] = useState(null);//取得した天気データ
+  const [loading, setLoding] = useState(false);
+  const [error, setError] = useState("")
 
-function App() {
+  const handleSearch = async() => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: 520, margin: "40px auto", fontFamily: "sans-serif" }}>
+      <h1>Weather App</h1>
+      <div style = {{ display: "flex", gap: 8}}> 
+        <input
+        value = {city}
+        onChange = {(e) => setCity(e.taeget.value)}
+        placeholder= "City(e.g. Tokyo)"
+        style={{flex: 1, pandding: 10}}
+        />
+      <button onClick= {handleSearch} style ={{ pandding : "10px 14px"}}>
+        Search
+      </button>
     </div>
+  <div style = {{marginTop: 16}}>
+    {loading && <p>Loading...</p>}
+    {error && <p style ={{ color: " crimson"}}> {error}</p>}
+
+    {weather && !loading &&(
+      <div style={{ border: "1px solid #add", padding: 12, borderRadius: 8}}>
+        <h2 style={{margin: "0 0 8px"}}>{weather.city}</h2>
+        <p style={{ margin: 0}}> Temperature: {weather.temp}°C</p>
+        <p style={{ margin: 0}}> Wind: {weather.wind}m/s</p>
+        <p style={{ margin:0}} > Weather code: {weather.code}</p>
+      </div>
+    )}
+  </div>
+</div>
   );
 }
-
-export default App;
